ddc603c9980e3d4bb1301fc83414c00353e26e9b
Caitlyn Nguyen <63687625+caitlynmn@users.noreply.github.com>
2022-04-16 16:49:28 -0400

    Add variable class file

---
+++ Variable_Class.py
@@ -0,0 +1,30 @@
+"""Creating a Variable class."""
+
+
+class Variable:
+    """Variable class."""
+
+    def __init__(self, name: str, values: list):
+        """Initialize."""
+        self.name = name
+        self.values = values
+
+    @property
+    def type(self):
+        """Variable type."""
+        return self._type
+
+    @type.setter
+    def type(self, var_type: str):
+        """Setter for variable type."""
+        self._type = var_type
+
+    @property
+    def x_or_y(self):
+        """Variable classification as x or y."""
+        return self._x_or_y
+
+    @x_or_y.setter
+    def x_or_y(self, x_or_y_type: str):
+        """Setter for variable classification."""
+        self._x_or_y = x_or_y_type
